0af39d9d641c16ba333b63fb2811d3953fefbc0e
jenniferzoey <jenniferzoey@gmail.com>
2019-08-07 13:02:26 -0400

    made brewery logos on profile page link to brewery show page

--- app/javascript/react/components/VisitTile.js
+++ app/javascript/react/components/VisitTile.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const VisitTile = (props) => {
 
   return (
     <div className="single-visit-brew-tile">
-
       <div className="breweryVisitTileContents">
-        <img src={props.visit.brewery_logo} />
-        <p> {props.visit.brewery_name} </p>
+        <Link to={`/breweries/${props.visit.brewery_id}`}>
+            <img src={props.visit.brewery_logo} />
+            <p> {props.visit.brewery_name} </p>
+        </Link>
       </div>
 
     </div>
